54ba632693010d23cd165c92d47b2917b75f754c
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-06 21:04:06 -0500

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length > 0:
+        fib_list.append(0)
+    if length >= 2:
+        fib_list.append(1)
+        for i in range(2, length):
+            fib_list.append(fib_list[-2] + fib_list[-1])
+    print (fib_list)
+
+    
